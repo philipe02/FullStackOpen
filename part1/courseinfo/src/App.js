1c152083e4a198c846868dbc2f857522e551eb46
Philipe Santos <philipe_02@hotmail.com>
2022-03-18 14:09:15 -0300

    1.4 step4: Changing content formart to array

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,19 +1,20 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
 
-  const content = [part1, part2, part3]
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   const Header = ({ course = '' }) => {
     return <h1>{course}</h1>
@@ -27,15 +28,15 @@ const App = () => {
     return content.map(item => <Part info={item} />)
   }
 
-  const Total = ({ exercises = [] }) => {
-    return <p>{exercises.reduce((previousValue, currentValue) => previousValue + currentValue)}</p>
+  const Total = ({ content = [] }) => {
+    return <p>{content.map(item => item.exercises).reduce((previousValue, currentValue) => previousValue + currentValue, 0)}</p>
   }
 
   return (
     <div>
       <Header course={course} />
-      <Content content={content} />
-      <Total exercises={content.map(item => item.exercises)} />
+      <Content content={parts} />
+      <Total content={parts} />
     </div>
   )
 }
