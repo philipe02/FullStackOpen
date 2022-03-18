7aa94413882affe47d90fd6c9e837c04f58fe54f
Philipe Santos <philipe_02@hotmail.com>
2022-03-18 14:02:48 -0300

    1.3 step3: Changing content format

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,22 +1,26 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
-  const content = [{ part: part1, exercise: exercises1 }, { part: part2, exercise: exercises2 }, { part: part3, exercise: exercises3 }]
-
-  const exercises = [exercises1, exercises2, exercises3]
+  const content = [part1, part2, part3]
 
   const Header = ({ course = '' }) => {
     return <h1>{course}</h1>
   }
 
   const Part = ({ info }) => {
-    return <p>{info.part} {info.exercise}</p>
+    return <p>{info.name} {info.exercises}</p>
   }
 
   const Content = ({ content = [] }) => {
@@ -31,7 +35,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content content={content} />
-      <Total exercises={exercises} />
+      <Total exercises={content.map(item => item.exercises)} />
     </div>
   )
 }
