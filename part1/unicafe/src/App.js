fcde3fa2217d6a31b425c2df9371c343313816d8
Philipe Santos <philipe_02@hotmail.com>
2022-03-27 11:09:59 -0300

    1.6 step1: Creating unicafe app with simple reviews

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+const Title = ({ text }) => <h1>{text}</h1>
+
+const Button = ({ label, onClick }) => <button onClick={onClick}>{label}</button>
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const incrementGood = () => setGood(good + 1);
+  const incrementNeutral = () => setNeutral(neutral + 1);
+  const incrementBad = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <Title text="give feedback" />
+      <section>
+        <Button onClick={incrementGood} label="good" />
+        <Button onClick={incrementNeutral} label="neutral" />
+        <Button onClick={incrementBad} label="bad" />
+      </section>
+      <Title text="statistics" />
+      <section>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+      </section>
+    </div>
+  )
+}
+
+export default App
