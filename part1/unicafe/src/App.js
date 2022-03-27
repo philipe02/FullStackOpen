18cf119e7aaf60d06fe1a0c30c8f09beabfb688c
Philipe Santos <philipe_02@hotmail.com>
2022-03-27 11:23:19 -0300

    1.8 step8: Creating Statistics component

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,6 +4,25 @@ const Title = ({ text }) => <h1>{text}</h1>
 
 const Button = ({ label, onClick }) => <button onClick={onClick}>{label}</button>
 
+const Statistics = ({ good, neutral, bad }) => {
+  const getTotalReviews = () => good + neutral + bad;
+  const getAverage = () => {
+    return getTotalReviews() ? (good - bad) / getTotalReviews() : 0;
+  }
+  const getPositivePercentage = () => {
+    return getTotalReviews() ? `${(good / getTotalReviews()) * 100}` : 0;
+  }
+
+  return <section>
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+    <p>all {getTotalReviews()}</p>
+    <p>average {getAverage()}</p>
+    <p>positive {getPositivePercentage()}</p>
+  </section>
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -13,9 +32,6 @@ const App = () => {
   const incrementGood = () => setGood(good + 1);
   const incrementNeutral = () => setNeutral(neutral + 1);
   const incrementBad = () => setBad(bad + 1);
-  const getTotalReviews = () => good + neutral + bad;
-  const getAverage = () => (good - bad) / getTotalReviews();
-  const getPositivePercentage = () => `${(good / getTotalReviews()) * 100}`
 
   return (
     <div>
@@ -26,14 +42,7 @@ const App = () => {
         <Button onClick={incrementBad} label="bad" />
       </section>
       <Title text="statistics" />
-      <section>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {getTotalReviews()}</p>
-        <p>average {getAverage()}</p>
-        <p>positive {getPositivePercentage()}</p>
-      </section>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
